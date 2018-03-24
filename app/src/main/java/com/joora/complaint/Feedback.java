e8744579200c14160d348f6a79339dc75f946c74
vineets740 <vineets740@gmail.com>
2018-03-24 17:43:13 +0530

    Created Feedback Page

---
+++ app/src/main/java/com/joora/complaint/Feedback.java
@@ -0,0 +1,17 @@
+package com.joora.complaint;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vineet on 24/3/18.
+ */
+
+public class Feedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feedback_layout);
+    }
+}
