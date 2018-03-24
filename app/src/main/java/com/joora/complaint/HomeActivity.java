3d2d4985b38269b887b026f0be73d1e9d3bcd9d5
shivamsoods <shivam.sood708@gmail.com>
2018-03-24 17:22:41 +0530

    Created Registeration , Splash Layout

--- app/src/main/java/com/joora/complaint/HomeActivity.java
+++ app/src/main/java/com/joora/complaint/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.joora.complaint;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
+
 }
---
+++ app/src/main/java/com/joora/complaint/Splash.java
@@ -0,0 +1,24 @@
+package com.joora.complaint;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT=4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent=new Intent(Splash.this,HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
